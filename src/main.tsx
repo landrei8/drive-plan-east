4f7bb33a4bb74128331bb33156f2e422287fb1f5
Lucian Andrei <andi@MacBook-Air-Lucian.local>
2026-03-23 19:30:54 +0200

    Final push from PC to GitHu3b

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-<<<<<<< HEAD
-=======
 import "leaflet/dist/leaflet.css";
->>>>>>> da00e7c (Connecting local files to GitHub)
 
 createRoot(document.getElementById("root")!).render(<App />);
